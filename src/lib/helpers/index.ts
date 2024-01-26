63ab668e68367b9aee59e56e8953b14677bbcbc5
Vedant Chainani <vedantchainani1084@gmail.com>
2024-01-26 17:04:17 +0530

    remove: format menu function

--- src/lib/helpers/index.ts
+++ src/lib/helpers/index.ts
@@ -1,7 +1,3 @@
-// 11:00:00+05:30 - 23:00:00+05:30
-
-import { MenuItem } from '~/types';
-
 export const formatTime = (timeString: string): string => {
   // Parse the input time string
   const [, hours, minutes, , timezoneOffset] =
@@ -25,20 +21,4 @@ export const formatTime = (timeString: string): string => {
   }).format(originalTime);
 
   return localTime;
-};
-
-export const formatMenu = (menu: MenuItem[]) => {
-  const sortedMenu = menu.reduce(
-    (acc, item) => {
-      if (!acc[item.category!]) {
-        acc[item.category!] = [];
-      }
-
-      acc[item.category!]!.push(item);
-      return acc;
-    },
-    {} as Record<string, MenuItem[]>
-  );
-
-  return sortedMenu;
 };
